180495803154076f2f7da5a88e4c3f1f29d3f04f
Szabados Dávid <david.szabados@webapix.hu>
2021-03-09 20:23:49 +0100

    basic design

---
+++ resources/views/template.blade.php
@@ -0,0 +1,42 @@
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>My Title</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+</head>
+<body>
+<div class="container pt-3">
+    <div class="row">
+        <div class="col-12 col-md-6">
+            <div class="card shadow-sm text-">
+                <div class="card-header">
+                    Title
+                </div>
+                <div class="card-body">
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus animi culpa dolor, enim error eum necessitatibus neque nostrum nulla perspiciatis provident quam quia reprehenderit saepe sint sit ut vel voluptatem.
+                </div>
+                <div class="card-footer">
+                    Copyright &copy; 2021
+                </div>
+            </div>
+        </div>
+        <div class="col-12 col-md-6">
+            <div class="card shadow-sm">
+                <div class="card-header bg-primary text-white">
+                    Title
+                </div>
+                <div class="card-body">
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus animi culpa dolor, enim error eum necessitatibus neque nostrum nulla perspiciatis provident quam quia reprehenderit saepe sint sit ut vel voluptatem.
+                </div>
+                <div class="card-footer">
+                    Copyright &copy; 2021
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+</body>
+</html>
